3378a19cdfaa2b2939cf4e64d66d2de078e4a8f0
Saulo Costa <saulotarsobc@gmail.com>
2023-04-15 11:33:41 -0300

    :fire:

---
+++ type_for_me.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key, Controller
+from time import sleep
+import sys
+import json
+
+""" vars """
+bot = Controller()
+
+# get args
+cong = json.loads(sys.argv[1])
+report = json.loads(sys.argv[2])
+form = json.loads(report['data'])
+# get args
+
+sleep(5)
+
+def fillHeader():
+    for i in [f"{cong['name']} - {cong['number']}", cong['city'], cong['state'], report['month'], report['year']]:
+        bot.press(Key.delete)
+        bot.type(str(i))
+        bot.press(Key.tab)
+        # sleep(.1)
+
+
+def fillForm():
+    for i in form:
+        bot.press(Key.delete)
+        bot.type(str(i))
+        bot.press(Key.tab)
+        # sleep(.1)
+
+
+if __name__ == '__main__':
+    fillHeader()
+    fillForm()
